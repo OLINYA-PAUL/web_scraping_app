34580ed10012f6b84d675111e375ecce9b28b300
OLINYA-PAUL <olinyacpaul7849@gmail.com>
2023-12-13 17:12:33 +0100

    web scraping app

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['mongoose']
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+      },
+    ],
+   
+  }
+
+}
+
+module.exports = nextConfig
